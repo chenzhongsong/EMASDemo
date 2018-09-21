a028afe6fb7f0fb86876e14f1952d7f256ebe801
peng.zhao <peng.zhao@alibaba-inc.com>
2018-09-21 15:15:27 +0800

    gen_weex_scaffold.sh -> gen_scaffold.sh

--- gen_scaffold.sh
+++ gen_scaffold.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Weex脚手架生成脚本
+#脚手架生成脚本
 #脚本依赖：
 #1. mac系统必须使用GUN的sed，使用命令安装gnu-sed到/usr/local/bin/sed，替换系统FreeBSD的sed. brew install gnu-sed --with-default-names
 #   安装完成后执行sed应该看到“GNU sed home page”几个关键字，如果不是说明还是使用的macos系统/usr/bin/sed，需要将/usr/local/bin加入$PATH且在/usr/bin之前
-#2. 帮助文档 ./gen_weex_scaffold.sh -h
-#3. 命令示例：./gen_weex_scaffold.sh -BUNDLE_ID my.bundle.id -SDK_CONFIG_APP_KEY appkey -SDK_CONFIG_APP_SECRET appsecret -SDK_CONFIG_CHANNEL_ID 1001@POC_iOS_1.0.0 -SDK_CONFIG_USE_HTTP false -SDK_CONFIG_ACCS_DOMAIN accs.com -SDK_CONFIG_MTOP_DOMAIN mtop.com -SDK_CONFIG_ZCACHE_PREFIX http://zcache.com/prefex -SDK_CONFIG_HOTFIX_URL http://hotfix.com -SDK_CONFIG_HA_OSS_BUCKET ha-oss-bucket -SDK_CONFIG_HA_ADASH_DOMAIN adash.com -APP_NAME myapp -MAVEN_BASE_GROUP com.my -WEEX_UI_SDK 1 -WEEX_BUSINESS_COMPONENTS 1 -WEEX_BUSINESS_CHARTS 1 -WEEX_PAGE_TAB_SIZE 5
+#2. 帮助文档 ./gen_scaffold.sh -h
+#3. 命令示例：./gen_scaffold.sh -BUNDLE_ID my.bundle.id -SDK_CONFIG_APP_KEY appkey -SDK_CONFIG_APP_SECRET appsecret -SDK_CONFIG_CHANNEL_ID 1001@POC_iOS_1.0.0 -SDK_CONFIG_USE_HTTP false -SDK_CONFIG_ACCS_DOMAIN accs.com -SDK_CONFIG_MTOP_DOMAIN mtop.com -SDK_CONFIG_ZCACHE_PREFIX http://zcache.com/prefex -SDK_CONFIG_HOTFIX_URL http://hotfix.com -SDK_CONFIG_HA_OSS_BUCKET ha-oss-bucket -SDK_CONFIG_HA_ADASH_DOMAIN adash.com -APP_NAME myapp -MAVEN_BASE_GROUP com.my -WEEX_UI_SDK 1 -WEEX_BUSINESS_COMPONENTS 1 -WEEX_BUSINESS_CHARTS 1 -WEEX_PAGE_TAB_SIZE 5
 
 set -e
 
@@ -36,7 +36,7 @@ WEEX_PAGE_TAB_SIZE=""
 
 #打印帮助文档
 printHelp() {
-    echo "Weex scaffold generate script."
+    echo "scaffold generate script."
     echo
     echo "options:"
     echo "   -h help."
@@ -222,7 +222,9 @@ checkParameters
 modifyNativeSDk
 
 #2. 发布包名修改
+echo "开始修改发布包名..."
 sed -i "/>CFBundleIdentifier</{n; s/<string>.*/<string>$BUNDLE_ID<\/string>/g; }" EMASDemo/Info.plist
+echo "修改发布包名完成."
 
 #3. weex外围sdk相关配置
 modifyWeexSDK
