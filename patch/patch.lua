bcb3187ac71a480646fb8d925e0c66a933b96a8e
魏晓堃 <filocore@IT-C02V127KG8WN.local>
2019-11-04 10:38:14 +0800

    fixed: clean proj

--- patch/patch.lua
+++
@@ -1,4 +0,0 @@
-interface{"HFXTestClass"}
-function output(self)
-return ('[TestClass] This is replaced output. chantu test again')
-end
--- patch/test.lua
+++
@@ -1,4 +0,0 @@
-interface{"TestClass"}
-function output(self)
-return ('[TestClass] This is replaced output. by chantu')
-end
