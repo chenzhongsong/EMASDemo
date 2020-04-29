d2aec1c0c3e66aacd8d304f5fbee9d558c607cd0
魏晓堃 <wb-wxk416923@alibaba-inc.com>
2020-04-29 10:59:22 +0800

    pushcentersdk 升级到 10.0.10 支持透传消息

--- EMASDemo/AppDelegate.m
+++ EMASDemo/AppDelegate.m
@@ -309,6 +309,7 @@
     
     // register notification setting
     [self registerNotificationSetting];
+    [self registerMsgReceive];
 }
 
 - (void)registerNotificationSetting {
@@ -335,6 +336,25 @@
         
         NSLog(@"[APNS] registerNotificationSetting");
     }
+}
+
+#pragma mark --- 透传消息
+
+- (void) registerMsgReceive {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMessageReceived:) name:@"EMASACCSReceiveMessageNotification" object:nil]; // 注册
+}
+
+- (void)onMessageReceived:(NSNotification *)notification {
+    NSDictionary *userInfo = notification.userInfo;
+    NSLog(@">>>>>>> [AGOO MESSAGE]: %@", userInfo);
+    NSString *text = [userInfo description];
+    if (!text ) {
+        text = @"消息解析失败!";
+    }
+    
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"AGOO 消息" message:text delegate:self cancelButtonTitle:@"确认" otherButtonTitles:nil, nil];
+    [alert show];
 }
 
 //-- 高可用
